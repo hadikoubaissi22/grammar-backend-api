ca367f4cd83f3f0bf94af871ecee5618e3aa97be
hadikoubaissi22 <hadikoubaissi2@gmail.com>
2025-09-20 13:46:32 +0300

    Register

--- index.js
+++ index.js
@@ -8,28 +8,11 @@ import { authMiddleware } from "./middleware/auth.js";
 dotenv.config();
 
 const app = express();
-
-// Configure CORS to allow specific origins
-const allowedOrigins = [
-  'http://localhost:3000', // Your local React app
-  'https://grammar-master-weld.vercel' // Replace with your actual frontend Vercel URL
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions)); // Use the configured options
-
+app.use(cors());
 app.use(express.json());
 
 // Routes
+// app.use('/lessons', lessonsRouter);
 app.use("/lessons", authMiddleware, lessonsRouter);
 app.use('/api', authRouter);
 

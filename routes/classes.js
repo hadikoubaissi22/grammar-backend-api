cd3f2b658e795f25be92311b2d51181160cd5998
hadikoubaissi22 <hadikoubaissi2@gmail.com>
2025-09-29 13:15:36 +0300

    Add Class

--- routes/classes.js
+++ routes/classes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // routes/classes.js
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM classes ORDER BY id ASC');
+    const result = await pool.query('SELECT * FROM classes where isdeleted=0 ORDER BY id ASC');
     res.json({ classes: result.rows }); // ✅ wrap in { classes: [] }
   } catch (err) {
     console.error('Error fetching classes:', err.message);
